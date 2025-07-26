24afc4cd1ac6d4068c9df639780599b809661c5d
Andrew Cheetham <andrewcheetham99@hotmail.com>
2025-07-26 08:45:58 +0100

    A test push

--- Code.js
+++ Code.js
@@ -7,7 +7,7 @@ function doGet() {
 function onAvailabilityFinderClick() {
   Logger.log('Availability Finder button clicked');
   // Add your availability finder logic here
-  return 'Availability Finder functionality will be implemented here';
+  return 'Availability Finder functionality will be implemented here x';
 }
 
 function onSkipLevelManagerClick() {
