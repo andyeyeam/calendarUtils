12909a339c3081575b5e6f2eb7b0ae1abc097914
Andrew Cheetham <andrewcheetham99@hotmail.com>
2025-07-26 08:31:58 +0100

    Initial commit

---
+++ Code.js
@@ -0,0 +1,17 @@
+function doGet() {
+  return HtmlService.createHtmlOutputFromFile('Index')
+    .setTitle("Andy's Calendar Utilities")
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+}
+
+function onAvailabilityFinderClick() {
+  Logger.log('Availability Finder button clicked');
+  // Add your availability finder logic here
+  return 'Availability Finder functionality will be implemented here';
+}
+
+function onSkipLevelManagerClick() {
+  Logger.log('Skip Level Manager button clicked');
+  // Add your skip level manager logic here
+  return 'Skip Level Manager functionality will be implemented here';
+}
